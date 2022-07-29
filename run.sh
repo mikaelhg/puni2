5778193ac613f7b8c66bb639828fd88b8538ee76
Mikael Gueck <gumi@iki.fi>
2022-07-29 16:08:24 +0300

    configuration

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Main entrypoint which does everything inside a Docker container
+
+GRAALVM_VERSION=${GRAALVM_VERSION:-'22.2.0'}
+PLANTUML_VERSION=${PLANTUML_VERSION:-'1.2022.6'}
+JLATEXMATH_VERSION=${JLATEXMATH_VERSION:-'1.0.7'}
+
 docker run -it --rm -w /build -v $PWD:/build/scripts:ro --entrypoint=/bin/bash \
-  ghcr.io/graalvm/graalvm-ce:22.2.0 \
+  -e PLANTUML_VERSION=${PLANTUML_VERSION} \
+  -e JLATEXMATH_VERSION=${JLATEXMATH_VERSION} \
+  ghcr.io/graalvm/graalvm-ce:${GRAALVM_VERSION} \
   -c "scripts/install.sh && scripts/download.sh && scripts/build.sh && scripts/test.sh"
