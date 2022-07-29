867e56bdbf11dba7689fdcd6bdf287713df7651b
Mikael Gueck <gumi@iki.fi>
2022-07-29 21:10:32 +0300

    dox

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Test the binary
+
+PLANTUML_VERSION=${PLANTUML_VERSION:-'1.2022.6'}
+
+./out/plantuml-${PLANTUML_VERSION}-glibc-x86_64 -headless -o out-native PlantUML-master/*/*.puml
